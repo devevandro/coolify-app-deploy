c3451ccad275b8501461f35ea3a0a6b0e2f4dedc
devevandro <dev.evandro@gmail.com>
2025-02-04 00:25:45 -0300

    fix: update logging to check for null value of secrets instead of 'undefined'

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ export const run = async () => {
       },
     });
 
-    console.log(secrets === 'undefined', 'CACETE');
+    console.log(secrets === null, 'CACETE');
     if (secrets !== undefined) {
       const secretsParsed =
         typeof secrets === "string" ? JSON.parse(secrets) : secrets;
