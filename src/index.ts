d577c03d01d9672a3bac424fe07e2055ad24d8b6
devevandro <dev.evandro@gmail.com>
2025-02-03 23:43:02 -0300

    refactor: streamline error handling and logging in deployment logic

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,39 @@
 import axios, { AxiosInstance } from "axios";
 import { getInput, setFailed } from "@actions/core";
 
-const errorConstructor = (message: string): never => {
-  throw new Error(message);
-};
-
-const logMessage = (message: string): void => {
-  console.log(message);
-};
-
-const baseApi = (coolifyUrl: string, coolifyToken: string): AxiosInstance => {
-  return axios.create({
-    baseURL: coolifyUrl,
-    headers: {
-      Authorization: `Bearer ${coolifyToken}`,
-      "Content-Type": "application/json",
-    },
-  });
-};
-
 export const run = async () => {
   try {
     const coolifyUrl = getInput("coolifyUrl");
     const coolifyToken = getInput("coolifyToken");
     const appUuid = getInput("coolifyAppUuid");
     const secrets = getInput("secrets") || "{}";
-    const secretToExclude = getInput("secrets") || [] as string[];
+    const secretToExclude = getInput("secrets") || ([] as string[]);
 
     if (!coolifyUrl || !coolifyToken || !appUuid) {
-      errorConstructor("Missing required environment variables");
+      throw new Error("Missing required environment variables");
     }
 
     const secretsParsed =
       typeof secrets === "string" ? JSON.parse(secrets) : secrets;
+    const convertedJsonToArray = Object.entries(secretsParsed)
+      .filter(([key]) => !secretToExclude.includes(key))
+      .map(([key, value]) => ({
+        key,
+        value,
+      }));
 
-    const api = baseApi(coolifyUrl, coolifyToken);
-    console.log("secretsParsed", secretsParsed);
+    const api = axios.create({
+      baseURL: coolifyUrl,
+      headers: {
+        Authorization: `Bearer ${coolifyToken}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-    if (secretsParsed && Object.keys(secretsParsed).length > 0) {
-      logMessage("Updating environment variables...");
-      const secretsConverted = Object.entries(secretsParsed)
-        .filter(([key]) => !secretToExclude.includes(key))
-        .map(([key, value]) => ({
-          key,
-          value,
-        }));
+    if (secretsParsed.length > 0) {
+      console.log("Updating environment variables...");
       const body = {
-        data: secretsConverted,
+        data: convertedJsonToArray,
       };
       const envUpdate = await api.patch(
         `/applications/${appUuid}/envs/bulk`,
@@ -54,20 +41,20 @@ export const run = async () => {
       );
 
       if (envUpdate.status !== 201) {
-        errorConstructor("Failed to update environment variables");
+        throw new Error("Failed to update environment variables");
       }
 
-      logMessage("Updated environment variables successfully!");
+      console.log("Updated environment variables successfully!");
     }
 
-    logMessage("Deploying application...");
-    const { status } = await api.post(`/deploy?uuid=${appUuid}`);
+    console.log("Restarting application...");
+    const restart = await api.post(`/deploy?uuid=${appUuid}`);
 
-    if (status !== 200) {
-      errorConstructor("Failed to restart application");
+    if (restart.status !== 200) {
+      throw new Error("Failed to restart application");
     }
 
-    logMessage("Deploy completed successfully!");
+    console.log("Deploy completed successfully!");
   } catch (error) {
     setFailed((error as Error)?.message ?? "Unknown error");
     throw error;
